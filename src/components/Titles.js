90d01b999a4303046f572939ed3a54313462eee8
T.J <t.ruggles@live.ca>
2019-03-19 04:35:46 -0400

    Adding Titles component

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Titles = () => (
+    <div>
+        <h1>Weather App</h1>
+        <p>Helps you find weather conditions wherever you are...</p>
+    </div>
+)
+
+export default Titles
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react"
 
-import 'normalize.css';
+import "normalize.css"
 
-const App = () => (
-    <div>
-        <h2>Anything I put here will show up in our local dev server except it'll look like shit cause it's not styled yet...</h2>
-        <p>Changes show up automatically like this...</p>
-    </div>
-)
+const App = () => <div>Hello World.</div>
 
-export default App;
+export default App
