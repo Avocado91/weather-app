3eea43ff66acbfe70db8f0069738efeae3dd681b
T.J <t.ruggles@live.ca>
2019-03-19 04:41:23 -0400

    Adding Form component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
 import React from "react"
+import Titles from "../components/Titles"
+import Form from "../components/Form"
 
 import "normalize.css"
 
-const App = () => <div>Hello World.</div>
+const App = () => (
+    <div>
+        <Titles />
+        Hello World.
+        <Form />
+    </div>
+)
 
 export default App
