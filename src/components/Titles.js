0e8665ba7762e488ea6f9d3771d6effede734856
T.J <t.ruggles@live.ca>
2019-03-20 22:47:52 -0400

    Fixing subtitle

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,9 +3,9 @@ import React from "react"
 const Titles = () => (
     <div>
         <div>
-            <h1 className="title-container__title">Weather Scanner </h1>
+            <h1 className="title-container__title">Weather App</h1>
             <p className="title-container__subtitle">
-                Helps you find weather conditions in cities...{" "}
+                Find weather conditions wherever you are...
             </p>
         </div>
     </div>
